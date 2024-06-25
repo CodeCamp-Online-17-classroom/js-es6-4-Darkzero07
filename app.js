b394cb29d6baaae696703f8ba9dcc3dda470d601
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-26 05:18:02 +0700

    js lab 148

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+// จงรวม nums1 และ num2 เข้าด้วยกัน
+// จงสร้าง array ที่ได้จากการนำ nums1 และ nums2 มารวมกัน แต่ให้เพิ่ม 5 เข้าไปก่อน nums1 และเพิ่ม -6, -1 เข้าไประหว่าง nums1 กับ nums2
+// หาผลรวมของทุก element ใน Array ที่ได้
+const nums1 = [1, -2, 3, 4];
+const nums2 = [8, 3, -8, 1];
+
+const combinedArray = [5, ...nums1, -6, -1, ...nums2];
+console.log(combinedArray);
+// [5, 1, -2, 3, 4, -6, -1, 8, 3, -8, 1]
+
+const sum = combinedArray.reduce((acc, num) => acc + num);
+console.log(sum);
